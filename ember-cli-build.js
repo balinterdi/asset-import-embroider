f927eea19381b6429891a25cc562254c0509160d
Balint Erdi <balint@balinterdi.com>
2023-10-06 12:47:58 +0200

    Make the image import work

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,27 +3,25 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    // autoImport: {
-    //   forbidEval: true,
-    //   webpack: {
-    //     module: {
-    //       rules: [
-    //         {
-    //           test: /\.(png|jpg|gif|svg|webp)$/i,
-    //           type: 'asset/resource',
-    //           generator: {
-    //             filename: 'assets/images/[name].[contenthash][ext][query]',
-    //           },
-    //         },
-    //       ],
-    //     },
-    //   },
-    // },
-  });
+  const app = new EmberApp(defaults, {});
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\.(png|jpg|gif|svg|webp)$/i,
+              type: 'asset/resource',
+              generator: {
+                filename: 'assets/images/[name].[contenthash][ext][query]',
+              },
+            },
+          ],
+        },
+      },
+    },
     skipBabel: [
       {
         package: 'qunit',
