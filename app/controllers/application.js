9bf3f3bd16fdae86009bf6b2dd68b705edae206a
Balint Erdi <balint@balinterdi.com>
2023-10-06 12:30:07 +0200

    Can't find image "module" in app/assets

---
+++ app/controllers/application.js
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+import ingestEmailSrc from 'asset-import/assets/images/ingest-email-light.svg';
+
+export default class ApplicationController extends Controller {
+  imageSrc = ingestEmailSrc;
+}
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,22 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    autoImport: {
+      forbidEval: true,
+      webpack: {
+        module: {
+          rules: [
+            {
+              test: /\.(png|jpg|gif|svg|webp)$/i,
+              type: 'asset/resource',
+              generator: {
+                filename: 'assets/images/[name].[contenthash][ext][query]',
+              },
+            },
+          ],
+        },
+      },
+    },
   });
 
   return app.toTree();
