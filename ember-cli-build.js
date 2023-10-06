57a5e7d4914e1cfb97484fe1a986d43516c1abfe
Balint Erdi <balint@balinterdi.com>
2023-10-06 12:43:05 +0200

    Switch to Embroider
    The build now throws an error but it's not a missing module one!
    
    It's Webpack complaining that it doesn't know how to import an image,
    as it's missing a loader

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,23 +4,31 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    autoImport: {
-      forbidEval: true,
-      webpack: {
-        module: {
-          rules: [
-            {
-              test: /\.(png|jpg|gif|svg|webp)$/i,
-              type: 'asset/resource',
-              generator: {
-                filename: 'assets/images/[name].[contenthash][ext][query]',
-              },
-            },
-          ],
-        },
-      },
-    },
+    // autoImport: {
+    //   forbidEval: true,
+    //   webpack: {
+    //     module: {
+    //       rules: [
+    //         {
+    //           test: /\.(png|jpg|gif|svg|webp)$/i,
+    //           type: 'asset/resource',
+    //           generator: {
+    //             filename: 'assets/images/[name].[contenthash][ext][query]',
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   },
+    // },
   });
 
-  return app.toTree();
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
+  // return app.toTree();
 };
